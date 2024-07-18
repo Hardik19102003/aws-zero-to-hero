1ac9b70b67892e650441d76030c86102d5167207
Hardik Patel <95542533+Hardik19102003@users.noreply.github.com>
2024-07-18 22:14:20 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -2,4 +2,7 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+containerid=$(docker ps -q)  # Get the ID of the running container
+if [ -n "$containerid" ]; then  # Check if there is a running container
+    docker rm -f $containerid  # Forcefully remove the running container
+fi
